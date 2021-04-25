852812feed415def053fd96d9f40bb388a2e4b31
Yuki Kanai <y.kaanai@gmail.com>
2021-04-25 23:23:52 +0900

    modify gen-del

--- internal/services/deljson/del_json.go
+++ internal/services/deljson/del_json.go
@@ -3,6 +3,8 @@ package deljson
 import (
 	"log"
 	"os"
+
+	"github.com/Kanai-Yuki/goapp_init/config"
 )
 
 type Service struct{}
@@ -11,8 +13,10 @@ func New() *Service {
 	return &Service{}
 }
 
+var jsonFileName string = "/goappinit.json"
+
 func (_ Service) Exec() {
-	err := os.Remove("goappinit2.json")
+	err := os.Remove(config.Conf.App.CurrentDir + jsonFileName)
 	if err != nil {
 		log.Fatalln("Remove goappinit.json error: ", err)
 	}
